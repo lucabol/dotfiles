ef704e9e3954da95f2689b00e161c3287d9f6f6a
Luca Bolognese <lucabol@microsoft.com>
2021-02-19 18:18:43 +0100

    Add zsh

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ ln -sf "$HOME/dotfiles/X11/.Xdefaults" "$HOME/.Xdefaults"
 
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+mkdir -p "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
+ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
