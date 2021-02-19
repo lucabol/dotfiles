2d26b9a3e425f8c3c4dd4b2e9623b73737f18000
Luca Bolognese <lucabol@microsoft.com>
2021-02-19 13:54:55 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim/undo"
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+
+ln -sf "$HOME/dotfiles/X11/.Xdefaults" "$HOME/.Xdefaults"
