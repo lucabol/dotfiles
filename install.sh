ee9b709ca3fbad07b7af49213708a86e23ff7f37
Luca Bolognese <lucabol@microsoft.com>
2021-02-19 15:37:16 +0100

    Add i3 config

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
 ln -sf "$HOME/dotfiles/X11/.Xdefaults" "$HOME/.Xdefaults"
+
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
